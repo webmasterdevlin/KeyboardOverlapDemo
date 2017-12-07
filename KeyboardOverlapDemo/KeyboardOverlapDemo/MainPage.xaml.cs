076863c1e538d4be8efedb95ee402682a5f60b81
webmasterdevlin <webmasterdevlin@gmail.com>
2017-12-07 23:21:33 +0800

    Add project files.

---
+++ KeyboardOverlapDemo/KeyboardOverlapDemo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KeyboardOverlapDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Test", "Tap is working.", "OK");
+        }
+    }
+}
